1b03f44afd19dbb83fbbbfd114943fd8e289ebab
mind28 <96925869+mind28@users.noreply.github.com>
2022-01-28 14:56:17 -0500

    added Pokemon array

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+let pokemonList = [{
+    name: 'Bulbasaur',
+    hiegth: 0.7,
+    weight: 6.9,
+    eggGroups: [
+      'Monster',
+      'Grass'
+    ],
+    abilites: [
+      'Chlorophyll',
+      'Overgrow'
+    ]
+  },
+  {
+    name: 'Charmander',
+    hiegth: 0.6,
+    weight: 8.5,
+    eggGroups: [
+      'Monster',
+      'Dragon'
+    ],
+    abilites: [
+      'Blaze',
+      'Solar-power'
+    ]
+  },
+  {
+    name: 'Butterfree',
+    hiegth: 1.1,
+    weight: 32,
+    eggGroups: [
+      'Bug',
+    ],
+    abilites: [
+      'Compoundeyes',
+      'Tinted-lens'
+    ]
+  }
+
+
+
+];
+
+
+console.log(pokemonList);
