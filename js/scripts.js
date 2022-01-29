dfff4d54958b011daa054926ffadc8cd74e749fe
mind28 <96925869+mind28@users.noreply.github.com>
2022-01-29 17:37:41 -0500

    removed commented code

--- js/scripts.js
+++ js/scripts.js
@@ -50,14 +50,3 @@ for (let i = 0; i < pokemonList.length; i++) {
   }
 
 }
-
-
-// for (let i = 0; i < person.length; i++) {
-//   if (person[i].age < 19 && person[i].age > 13) {
-//     console.log(person[i].name + " is a teenager");
-//   } else if (person[i].age < 13) {
-//     console.log(person[i].name + " is a child");
-//   } else {
-//     console.log(person[i].name + " is an adult");
-//   }
-// }
