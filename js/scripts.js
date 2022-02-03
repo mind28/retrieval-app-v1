614460d8350dc0b618232c5b57491fe15cb8d740
mind28 <96925869+mind28@users.noreply.github.com>
2022-02-03 14:58:09 -0500

    wrapped pokemonList array in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,72 @@
-let pokemonList = [{
-    name: 'Bulbasaur',
-    size: 0.7,
-    weight: 6.9,
-    eggGroups: [
-      'Monster',
-      'Grass'
-    ],
-    abilites: [
-      'Chlorophyll',
-      'Overgrow'
-    ]
-  },
-  {
-    name: 'Charmander',
-    size: 0.6,
-    weight: 8.5,
-    eggGroups: [
-      'Monster',
-      'Dragon'
-    ],
-    abilites: [
-      'Blaze',
-      'Solar-power'
-    ]
-  },
-  {
-    name: 'Butterfree',
-    size: 1.1,
-    weight: 32,
-    eggGroups: [
-      'Bug',
-    ],
-    abilites: [
-      'Compoundeyes',
-      'Tinted-lens'
-    ]
+// IIFE repo------------------------------------
+
+let pokemonRepository = (function() {
+  let pokemonList = [{
+      name: 'Bulbasaur',
+      size: 0.7,
+      weight: 6.9,
+      eggGroups: [
+        'Monster',
+        'Grass'
+      ],
+      abilites: [
+        'Chlorophyll',
+        'Overgrow'
+      ]
+    },
+    {
+      name: 'Charmander',
+      size: 0.6,
+      weight: 8.5,
+      eggGroups: [
+        'Monster',
+        'Dragon'
+      ],
+      abilites: [
+        'Blaze',
+        'Solar-power'
+      ]
+    },
+    {
+      name: 'Butterfree',
+      size: 1.1,
+      weight: 32,
+      eggGroups: [
+        'Bug',
+      ],
+      abilites: [
+        'Compoundeyes',
+        'Tinted-lens'
+      ]
+    }
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
 
+  function getAll() {
+    return pokemonList;
+  }
 
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
 
-];
+console.log(pokemonRepository.getAll()); // []
+pokemonRepository.add({
+  name: 'Pikachu',
+  size: 0.4
+});
+console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].size < 1) {
-    document.write("<p>" + pokemonList[i].name + " (" + pokemonList[i].size + ")" + "</p>");
+pokemonRepository.getAll().forEach(function(pokemon) {
+  if (pokemon.size < 1) {
+    document.write("<p>" + pokemon.name + ' ( ' + pokemon.size + ' )' + "</p>");
   } else {
-    document.write("<p>" + pokemonList[i].name + " (" + pokemonList[i].size + ")" + " - Wow that's big" +
-      "</p>");
+    document.write("<p>" + pokemon.name + ' ( ' + pokemon.size + ' )' + " WOW that's big" + "</p>");
   }
 
-}
+});
